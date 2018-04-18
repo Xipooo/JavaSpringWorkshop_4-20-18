0feaa43481f5e8691e14da99fded6bec83faf077
Steve Bishop <xipooo@gmail.com>
2018-04-18 14:15:07 -0700

    Fixed formatting

--- src/com/wozu/workshop/App.java
+++ src/com/wozu/workshop/App.java
@@ -6,11 +6,11 @@ public class App {
 
 	public static void main(String[] args) {
 		NameCollector nameCollector = new NameCollector();
-        nameCollector.AddName("Tina");
-        String[] names = nameCollector.GetNames();
-        int i = 1;
-        while(i++ <= names.length){
-            System.out.println(names[i]);
-        }
+		nameCollector.AddName("Tina");
+		String[] names = nameCollector.GetNames();
+		int i = 1;
+		while (i++ <= names.length) {
+			System.out.println(names[i]);
+		}
 	}
 }
